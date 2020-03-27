515e1d2098e127b6d490d1273a00e61f7719b0b9
amirxu <amirxu@outlook.com>
2020-03-27 19:21:44 +0800

    Initial Commit

---
+++ DataStructure/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  DataStructure
+//
+//  Created by amirx on 2020/3/27.
+//  Copyright © 2020 amirx. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
